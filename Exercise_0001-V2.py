04ed8e9f06e6c96d0756d47ebd256d9840763e74
ReliutionKaran <karanh.reliution@gmail.com>
2024-01-04 13:09:16 +0530

    [ADD] code

--- Exercise_0001-V2.py
+++ Exercise_0001-V2.py
@@ -1,10 +1,10 @@
 class Category:
-    def __init__(self, name, code, no_of_products, parent=None):
+    def __init__(self, name, code, no_of_products, parent=None, display_name=None):
         self.name = name
         self.code = code
         self.NOP = no_of_products
         self.parent = parent
-        self.display_name = None
+        self.display_name = display_name
         self.Products = []
       
     def generate_display_name(self):
@@ -14,26 +14,19 @@ class Category:
             self.display_name = self.name
         return self.display_name
     
-    def display_products_by_category(categories):
-        category_dict = {}
-
-        # Organize products by category name
-        for category in categories:
-            category_name = category.name
-            if category_name not in category_dict:
-                category_dict[category_name] = []
-
-            category_dict[category_name].extend(category.Products)
+    def add_product(self, product):
+        self.Products.append(product)
         
     def Category_info(categories):
-        for Category in categories:
-            print(f"Category: {Category.name}\nCode: {Category.code}\nNo. of Products: {Category.NOP}\n")
-            Category.generate_display_name()
-
+        for category in categories:
+            print(f"Category: {category.name}\nCode: {category.code}\nNo. of Products: {category.NOP}\n")
+            for product in category.Products:
+                print(f"Product: {product.name} (Code: {product.code}, Price: {product.price:.3f})")
+            print("\n")
    
 class Product(Category):
-    
     def __init__(self, name, code, category, price):
+        category.add_product(self)
         category.NOP += 1
         self.name = name
         self.code = code
@@ -46,7 +39,7 @@ class Product(Category):
         for i in range(0, len(Products)):
             for j in range(i+1, len(Products)):
                 if Products[i].price <= Products[j].price:
-                    Products[i].price, Products[j].price = Products[j].price, Products[i].price
+                    Products[i], Products[j] = Products[j], Products[i]
                     
 # Sort and print products based on price (Low to High)
     @staticmethod
@@ -54,7 +47,7 @@ class Product(Category):
         for i in range(0, len(Products)):
             for j in range(i+1, len(Products)):
                 if Products[i].price >= Products[j].price:
-                    Products[i].price, Products[j].price = Products[j].price, Products[i].price
+                    Products[i], Products[j] = Products[j], Products[i]
     
 # Search Products from code
     @staticmethod                
@@ -80,7 +73,7 @@ class Product(Category):
         found_products = Product.find_products(Products, target_code)
  
         if found_products:
-            print(f"Product found: {found_products.name} (Code: {found_products.code}, Category: {found_products.category}, Price: {found_products.price:.5f})")
+            print(f"Product found: {found_products.name} (Code: {found_products.code}, Category: {found_products.category}, Price: {found_products.price:.3f})")
         else:
             print(f"Product with code '{target_code}' not found.")
         
@@ -97,47 +90,42 @@ Gas = Category("Gas", "8", 0, Car)
 categories = [C1, C2, C3, Vehicle, Car, Petrol, Electric, Gas]
 
 
-P1 = Product("Laptop", "P001", C1, 70.000)
-P2 = Product("Smart Phone", "P002", C1, 10.000)
-P3 = Product("Coffie Maker", "P003", C2, 5.500)
-P4 = Product("Digital Camera", "P004", C1, 50.000)
-P5 = Product("Television", "P005", C1, 12.000)
-P6 = Product("Running Shoes", "P006", C3, 1.200)
-P7 = Product("Season Bat", "P007", C3, 4.000)
-P8 = Product("Kitchen Stove", "P008", C2, 1.500)
-P9 = Product("Toaster", "P009", C2, 1.600)
-P10 = Product("Cricket Kit", "P010", C3, 17.000)
+P1 = Product("Laptop", "P001", C1, 50)
+P2 = Product("Smart Phone", "P002", C1, 10)
+P3 = Product("Coffie Maker", "P003", C2, 5.5)
+P4 = Product("Digital Camera", "P004", C1, 50)
+P5 = Product("Television", "P005", C1, 12)
+P6 = Product("Running Shoes", "P006", C3, 1.2)
+P7 = Product("Season Bat", "P007", C3, 4)
+P8 = Product("Kitchen Stove", "P008", C2, 1.5)
+P9 = Product("Toaster", "P009", C2, 1.6)
+P10 = Product("Cricket Kit", "P010", C3, 17)
 
-P11 = Product("TATA Harier", "P011", Car, 15.00000)
-P12 = Product("TATA Safari", "P012", Car, 25.00000)
-P13 = Product("TATA Altroz", "P013", Car, 10.00000)
+P11 = Product("TATA Harier", "P011", Car, 150)
+P12 = Product("TATA Safari", "P012", Car, 250)
+P13 = Product("TATA Altroz", "P013", Car, 100)
 
-P14 = Product("Platina", "P014", Vehicle, 75.000)
-P15 = Product("Pulser", "P015", Vehicle, 90.000)
-P16 = Product("Honda Shine", "P016", Vehicle, 80.000)
+P14 = Product("Platina", "P014", Vehicle, 75)
+P15 = Product("Pulser", "P015", Vehicle, 90)
+P16 = Product("Honda Shine", "P016", Vehicle, 80)
 
-P17 = Product("Toyota Supra", "P017", Petrol, 99.00000)
-P18 = Product("Fortuner", "P018", Petrol, 50.00000)
-P19 = Product("Thar", "P019", Petrol, 25.00000)
+P17 = Product("Toyota Supra", "P017", Petrol, 990)
+P18 = Product("Fortuner", "P018", Petrol, 500)
+P19 = Product("Thar", "P019", Petrol, 250)
 
-P20 = Product("Nexon EV", "P020", Electric, 9.00000)
-P21 = Product("Hundai EV", "P021", Electric, 12.00000)
-P22 = Product("Maruti EV", "P022", Electric, 10.00000)
+P20 = Product("Nexon EV", "P020", Electric, 900)
+P21 = Product("Hundai EV", "P021", Electric, 120)
+P22 = Product("Maruti EV", "P022", Electric, 100)
 
-P23 = Product("Swift", "P023", Gas, 7.00000)
-P24 = Product("Alto", "P024", Gas, 4.50000)
-P25 = Product("Ecco", "P025", Gas, 4.00000)
+P23 = Product("Swift", "P023", Gas, 700)
+P24 = Product("Alto", "P024", Gas, 450)
+P25 = Product("Ecco", "P025", Gas, 400)
 
 Products = [P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, P19, P20, P21, P22, P23, P24, P25]
 
 C = Category.Category_info(categories)
 print(C)
-
 P = Product.Product_info(Products)
 P
-
 D = Category.generate_display_name(Petrol)
 print(D)
-
-E = Category.display_products_by_category(categories)
-print(E)
