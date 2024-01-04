0eddef622d9c354811e82325cd391bcbec69c864
ReliutionKaran <karanh.reliution@gmail.com>
2024-01-04 12:05:21 +0530

    [UPD] Code

--- Exercise_0001-V1.py
+++ Exercise_0001-V1.py
@@ -11,11 +11,11 @@ class Category:
    
 class Product(Category):
     def __init__(self, name, code, category, price):
-        category.NOP += 1
         self.name = name
         self.code = code
         self.category = category.name
         self.price = price
+        category.NOP += 1
                 
 # Sort and print products based on price (High to Low) 
     @staticmethod
@@ -23,7 +23,7 @@ class Product(Category):
         for i in range(0, len(Products)):
             for j in range(i+1, len(Products)):
                 if Products[i].price <= Products[j].price:
-                    Products[i].price, Products[j].price = Products[j].price, Products[i].price
+                    Products[i], Products[j] = Products[j], Products[i]
                     
 # Sort and print products based on price (Low to High)
     @staticmethod
@@ -31,7 +31,7 @@ class Product(Category):
         for i in range(0, len(Products)):
             for j in range(i+1, len(Products)):
                 if Products[i].price >= Products[j].price:
-                    Products[i].price, Products[j].price = Products[j].price, Products[i].price
+                    Products[i], Products[j] = Products[j], Products[i]
     
 # Search Products from code
     @staticmethod                
@@ -44,13 +44,13 @@ class Product(Category):
         print("Products sorted by price (High to Low):")        
         Product.sorted_products_high_to_low(Products)
         for product in Products:
-            print(f"{product.name} ({product.category}): {product.price:.3f}")
+            print(f"{product.name} ({product.category}): {product.price:}")
 
     #Low to High Sorting
         Product.sorted_products_low_to_high( Products)                
         print("\nProducts sorted by price (Low to High):")
         for product in Products:
-            print(f"{product.name} ({product.category}): {product.price:.3f}")
+            print(f"{product.name} ({product.category}): {product.price:}")
             
     #Searching of Product
         target_code = input("Enter the code: ")
@@ -66,20 +66,18 @@ C2 = Category("Appliances", "2", 0)
 C3 = Category("Sports", "3", 0)
 categories = [C1, C2, C3]
 
-P1 = Product("Laptop", "P01", C1, 70.000)
-P2 = Product("Smart Phone", "P02", C1, 10.000)
-P3 = Product("Coffie Maker", "P03", C2, 5.500)
-P4 = Product("Digital Camera", "P04", C1, 50.000)
-P5 = Product("Television", "P05", C1, 12.000)
-P6 = Product("Running Shoes", "P06", C3, 1.200)
-P7 = Product("Season Bat", "P07", C3, 4.000)
-P8 = Product("Kitchen Stove", "P08", C2, 1.500)
-P9 = Product("Toaster", "P09", C2, 1.600)
-P10 = Product("Cricket Kit", "P10", C3, 17.000)
+P1 = Product("Laptop", "P01", C1, 70.0)
+P2 = Product("Smart Phone", "P02", C1, 10.0)
+P3 = Product("Coffie Maker", "P03", C2, 5.5)
+P4 = Product("Digital Camera", "P04", C1, 50.0)
+P5 = Product("Television", "P05", C1, 12.0)
+P6 = Product("Running Shoes", "P06", C3, 1.2)
+P7 = Product("Season Bat", "P07", C3, 4.0)
+P8 = Product("Kitchen Stove", "P08", C2, 1.5)
+P9 = Product("Toaster", "P09", C2, 1.6)
+P10 = Product("Cricket Kit", "P10", C3, 17.0)
 Products = [P1, P2, P3, P4, P5, P6, P7, P8, P9, P10]
 
 C = Category.Category_info(categories)
 print(C)
-
-P = Product.Product_info(Products)
-P
+Product.Product_info(Products)
